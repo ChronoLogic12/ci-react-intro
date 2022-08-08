e56503d965dad8cfaa41d5ee678c9ef9d44aef69
ChronoLogic <msuttie12@hotmail.com>
2022-08-08 22:34:29 +0100

    Create conditionally rendering components. Extract and nest conditional components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 
 // import FunctionGreetingWithProps from "./components/FunctionalGreetingWithProps";
 // import StatefulGreeting from "./components/StatefulGreeting";
-import EventsClass from "./components/EventsClass";
-import EventsFunctional from "./components/EventsFunctional";
+// import EventsClass from "./components/EventsClass";
+// import EventsFunctional from "./components/EventsFunctional";
+// import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+// import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
 	return (
 		<div className="App">
-			<EventsClass />
-			<EventsFunctional />
+			<NestingComponents />
 		</div>
 	);
 }
