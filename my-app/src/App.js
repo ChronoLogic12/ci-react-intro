a02363eb34dfba80840a8d89d83ed23291fda4ee
ChronoLogic <msuttie12@hotmail.com>
2022-08-11 17:25:09 +0100

    use state with objects and arrays

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,12 +14,14 @@ import "./App.css";
 // import ControlledForm from "./components/ControlledForm";
 // import UncontrolledForm from "./components/UncontrolledForm";
 // import SearchBar from "./components/SearchBar";
-import ControlledFormHooks from "./components/ControlledFormHooks";
+// import ControlledFormHooks from "./components/ControlledFormHooks";
+// import UseStateWithArrays from "./components/UseStateWithArrays";
+import UseStateWithObjects from "./components/UseStateWithObjects";
 
 function App() {
 	return (
 		<div className="App">
-			<ControlledFormHooks />
+			<UseStateWithObjects />
 		</div>
 	);
 }
